47d7d039244bfbd703ce9f0bac913af7074bf57a
Akash302002 <aakashbansal302002@gmail.com>
2025-06-23 21:06:48 +0530

    edited routes auth

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 const mongoose=require('mongoose');
+require("dotenv").config()
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,33 @@
 const User = require('../models/userModel');
-
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 //Register a new user
 const registerUser =async (req, res) => {
     //TODO : check if user already exists, if not create a new user, store password as hashed value, and send a response back to the client.
     try {
         const { name, email, password } = req.body;
+        if(!name || !email || !password)
+            return res.status(400).json({msg:"Fill All Details",success:false})
+
+        const existingUser = await User.findOne({email:email})
+        
+        if(existingUser)
+            return res
+              .status(400)
+              .json({ msg: "User Already Signed Up.", success: false });
+        
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        if(!hashedPassword)
+            return res.status(400).json({msg:"Error in hashing password",success:false})
+        
+        const savedUser = await User.create({name,email,password:hashedPassword})
+
+        return res.status(200).json({msg:"User Registered Successfully.",success:true,user:savedUser})      
+
         
     } catch (error) {
-       next(error); 
+       return res.status(400).json({error:error.message})
     }
 }
 
@@ -15,15 +35,57 @@ const registerUser =async (req, res) => {
 //Login an existing user
 const loginUser = async (req, res) => {
 
-    //TODO : check if user exists, if yes, compare password with hashed password, and send a response back to the client.
+    
     //If user does not exist, send an error response.
     // If password is correct, generate a JWT token and send it back to the client.
     // If password is incorrect, send an error response.
     try {
-        const { email, password } = req.body;
-        
+      const { email, password } = req.body;
+
+      if (!email || !password)
+        return res
+          .status(400)
+          .json({ msg: "Fill All Details", success: false });
+
+      const isExist = await User.findOne({ email });
+
+    //TODO : check if user exists, if yes, compare password with hashed password, and send a response back to the client.
+      if (!isExist)
+        return res
+          .status(400)
+          .json({ msg: "User Not Signed Up.", success: false });
+
+      // Password compare
+      const isPassword = await bcrypt.compare(password,isExist.password) 
+      
+      if(!isPassword)
+        return res
+          .status(400)
+          .json({ msg: "Invalid Credentials. ", success: false });
+      
+      // generate token
+       const token = jwt.sign({userId: isExist?._id,role:isExist?.role},process.env.JWT_SECRET,{expiresIn:'1d'})
+       
+       if(!token)
+        return res
+          .status(400)
+          .json({ msg: "Error in Token Generation", success: false });
+       
+     return res
+       .cookie("Token", token, {
+         httpOnly: true,
+         secure: true,
+         sameSite: "strict",
+         maxAge:24 *60*60*1000
+       })
+       .status(200)
+       .json({ msg: "User Logged In Successfully ", success: true,token:token });   
+
     } catch (error) {
-       next(error); 
+        return res
+          .status(400)
+          .json({ error:error.message });
+
     }
 
  
@@ -36,25 +98,54 @@ const getUserProfile = async (req, res) => {
     try {
         const userId = req.params; // Assuming user ID is stored in req.user after authentication
         
+        if(!userId)
+            return res.status(400).json({msg:"Error in fetching ID ",success:false})
+
+        const existingUser = await User.findById({_id:userId})
+
+        if (!existingUser)
+          return res
+            .status(400)
+            .json({ msg: " User Not Found ", success: false });
+        
+         return res.status(200).json({msg:"User Details ",success:true,userDetails:existingUser})    
+
+
         
     } catch (error) {
-       next(error); 
+        return res.status(400).json({ error: error.message }); 
     }
 
  
 }
 
 //Verify Auth Token
-const verifyAuthToken = async (req, res) => {
+const verifyAuthToken = async (req, res,next) => {
 
     //TODO : verify the JWT token, if valid, send a success response, if not valid, send an error response.
     // You can use a library like jsonwebtoken to verify the token.
     try {
         const token = req.headers.authorization?.split(' ')[1]; // Assuming Bearer token format
         
+        if(!token)
+         return res
+        .status(400)
+        .json({msg:"Token not Found",success:false});
+
+        // verify 
+        const data = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(!data)
+            return res.status(400).json({ msg:"User Not Logged In ",success:false });
+
+        req.user = data
+
+         res.json({msg:"Token Verified Successfully",success:true}).status(200);
+
+        next()
         
     } catch (error) {
-       next(error); 
+        return res.status(400).json({ error: error.message });
     }
 
  
