b7a7209570e310ac40a9080ca56a619e69388d0d
unknown <atif251171@gmail.com>
2025-07-04 16:06:36 +0530

    changes for deploying to render v4

--- backend/routes/Auth2Routes.js
+++ backend/routes/Auth2Routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const router = express.Router();
@@ -47,45 +47,50 @@ router.get("/auth/google/callback", async (req, res) => {
 
     const user = userResponse.data;
 
-    
     console.log("User info:", user);
     //Check if user exists
-    
+
     const existingUser = await User.findOne({ email: user.email });
     if (existingUser) {
-    const token=jwt.sign({userId: existingUser._id, role: existingUser.role}, process.env.JWT_SECRET, {expiresIn: "15m"});
+      const token = jwt.sign(
+        { userId: existingUser._id, role: existingUser.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "15m" }
+      );
 
-    return res
-      .cookie("Token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV == "production"?true:false, // Set to true in production
-        sameSite: "strict",
-        maxAge: 24 * 60 * 60 * 1000,
-      })
-      .status(200)
-      .redirect(`http://localhost:5173/`);
-    }
-    else
-    {
-         const tokenPayload = {
-      email: user.email,
-      name: user.name,
-      googleId: user.sub || user.googleId,
-      picture: user.picture,
-    };
+      return res
+        .cookie("Token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV == "production" ? true : false, // Set to true in production
+          sameSite: "strict",
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .redirect(
+          process.env.NODE_ENV === "production" ? "/" : "http://localhost:5173/"
+        );
+      // .redirect(`http://localhost:5173/`);
+    } else {
+      const tokenPayload = {
+        email: user.email,
+        name: user.name,
+        googleId: user.sub || user.googleId,
+        picture: user.picture,
+      };
 
+      const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
+        expiresIn: "15m", // short-lived for pre-registration
+      });
 
-    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
-      expiresIn: "15m", // short-lived for pre-registration
-    });
-
-    // Redirect to frontend with token in query
-    res.redirect(`http://localhost:5173/user/register?token=${token}`);
+      // Redirect to frontend with token in query
+      res.redirect(
+        process.env.NODE_ENV === "production"
+          ? `/user/register?token=${token}`
+          : `http://localhost:5173/user/register?token=${token}`
+      );
     }
 
     // Send response to frontend
-   
-   
   } catch (err) {
     console.error(
       "Error during OAuth callback:",
