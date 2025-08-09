779e154bf5cff608dff4f1969120277ca0bc3bdc
atif2112011 <atif251171@gmail.com>
2025-08-09 15:47:32 +0530

    logo updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -95,9 +95,9 @@ function NavBar() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <div className="flex flex-col md:flex-row items-center justify-between gap-4 py-2 px-4 md:px-6 bg-white shadow-md relative transition-all duration-300 ease-in-out">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-4 py-2 px-4 md:px-8 bg-white shadow-md relative transition-all duration-300 ease-in-out">
         {/* Logo + Cart + Hamburger on Mobile */}
-        <div className="flex flex-row w-full md:w-auto justify-between items-center md:px-7">
+        <div className="flex flex-row w-full md:w-auto justify-between items-center md:pl-5 md:pt-1">
           <div className="flex items-center gap-3">
             <img src={logo} alt="Logo" className="h-10" />
             <img src={logoText} alt="Logo-text" className="h-8" />
